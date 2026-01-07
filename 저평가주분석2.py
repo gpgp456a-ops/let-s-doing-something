8c38a9c8581046dbe63557f419389409801106eb
gpgp456a-ops <gpgp456a@ajou.ac.kr>
2026-01-07 15:58:34 +0900

    Update 저평가주분석2.py

--- 저평가주분석2.py
+++ 저평가주분석2.py
@@ -8,7 +8,8 @@ import json
 import io
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta, date
-from pykrx import stock
+import pykrx
+import patch_pykrx
 import gspread
 from google.oauth2.service_account import Credentials
 from gspread_dataframe import set_with_dataframe
